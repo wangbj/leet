b2566c79fa1f4db5e0499e0b3aba3f59d3d68442
Baojun Wang <wangbj@gmail.com>
2015-12-21 14:21:15 -0800

    Update 303.java

--- 303.java
+++ 303.java
@@ -19,15 +19,6 @@ public class NumArray {
     public int sumRange(int i, int j) {
 	return sumCache[j] - sumCache[i] + ints[i];
     }
-    
-    public static void dumpArray(int[] arr)
-    {
-	for (int i = 0; i < arr.length; i++) {
-	    System.out.format("%d ", arr[i]);
-	}
-	System.out.println("\n");
-    }
-    
 }
 
 
@@ -35,21 +26,3 @@ public class NumArray {
 // NumArray numArray = new NumArray(nums);
 // numArray.sumRange(0, 1);
 // numArray.sumRange(1, 2);
-
-
-class Solution {
-    public static void main(String[] argv)
-    {
-	int arr[] = {1, 2, 3, 4, 5, 6};
-	int x;
-	
-	NumArray.dumpArray(arr);
-
-	NumArray num = new NumArray(arr);
-	
-	x = num.sumRange(0, 0);
-	System.out.format("%d\n", x);
-	x = num.sumRange(2, 3);
-	System.out.format("%d\n", x);	
-    }
-}
