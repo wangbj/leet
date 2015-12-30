0eb1b35d6326e526018809be7973eb4ea613d021
Baojun Wang <wangbj@gmail.com>
2015-12-29 17:53:06 -0800

    Update 227.hs

--- 227.hs
+++ 227.hs
@@ -1,37 +1,27 @@
+import Control.Monad
 import Data.Char
 import Data.Maybe
-
-data BinaryOp = Add
-              | Sub
-              | Mul
-              | Div
 
 fromDigit x
   | x >= '0' && x <= '9' = ord x - ord '0'
 
-readint s = if null s then Nothing else go Nothing (0, dropWhile isSpace s)
+readint s = go Nothing (0, dropWhile isSpace s)
   where go st (r, []) = st >> return (r, [])
         go st r@(res, (c:cs)) = case isDigit c of
           False -> st >> return r
           _ -> go (Just True) (res*10 + fromDigit c, cs)
 
-readop s =
-  let s' = dropWhile isSpace s
-  in if null s' then Nothing else
-       let (x:xs) = s'
-           tr '+' = Add
-           tr '-' = Sub
-           tr '*' = Mul
-           tr '/' = Div
-       in Just (tr x, xs)
+readop = uncons . dropWhile isSpace
+  where uncons [] = Nothing
+        uncons (x:xs) = Just (x, xs)
 
-acc lhs [] = Just lhs
-acc lhs s = readop s >>= \(op, s1) -> case op of
-  Add -> fmap (lhs + ) (eval s1)
-  Sub -> fmap (lhs - ) (eval s1)
-  _ -> readint s1 >>= \(rhs, s2) ->
-    case op of
-      Mul -> acc (lhs * rhs) (dropWhile isSpace s2)
-      Div -> acc (lhs `div` rhs) (dropWhile isSpace s2)
+acc lhs s = go lhs s `mplus` Just lhs
+  where go lhs s = readop s >>= \(op, s1) -> case op of
+          '+' -> fmap (lhs + ) (eval s1)
+          '-' -> fmap (lhs - ) (eval s1)
+          _ -> readint s1 >>= \(rhs, s2) ->
+            case op of
+              '*' -> acc (lhs * rhs) s2
+              '/' -> acc (lhs `div` rhs) s2
 
 eval s = readint s >>= \(lhs, s1) -> acc lhs (dropWhile isSpace s1)
