66069bfff91669272f0452a230fdcd8e32f89bda
Baojun Wang <wangbj@gmail.com>
2016-01-15 15:52:16 -0800

    Update 198.hs
    O(n) simple recursive (right fold), no dp required.

--- 198.hs
+++ 198.hs
@@ -1,9 +1,10 @@
 maxp [] = (Nothing, Nothing)
 maxp (x:[]) = (Just x, Nothing)
-maxp (x:y:[]) = if x > y then (Just x, Just y) else (Just y, Just y)
-maxp (x:y:zs) = case maxp (y:zs) of
-  (Just r1, Just r2) -> let x' = x + r2
-                            y' = r1
-                        in if x' > y' then (Just x', Just y') else (Just y', Just y')
+maxp (x:y:[])
+  | x > y = (Just x, Just y) 
+  | otherwise =  (Just y, Just y)
+maxp (x:xs) = case maxp xs of
+  (Just a, Just b) -> let x' = x + b; y' = a in
+    if x' > y' then (Just x', Just y') else (Just y', Just y')
 
-rob = uncurry max .  maxp
+rob = uncurry max . maxp
