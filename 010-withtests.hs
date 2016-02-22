9fdfd4a7ae01caaf586e7e0442eb6c339ae44494
Baojun Wang <wangbj@gmail.com>
2016-02-21 17:21:35 -0800

    Create 010-withtests.hs
    Add tests for 010.hs, including quickCheck & criterion. As the use case if much more simpler, the speed up compare to full regex parser is dramatic.
    
     	lower bound 	estimate 	upper bound
    OLS regression 	165 μs 	168 μs 	176 μs
    R² goodness-of-fit 	0.855 	0.944 	1.000
    Mean execution time 	166 μs 	177 μs 	209 μs
    Standard deviation 	27.9 μs 	49.1 μs 	96.4 μs
    
    Outlying measurements have severe (97.0%) effect on estimated standard deviation.
    match/2
    8
    7.8
    7.9
    8.1
    8.2
    8.3
    8.4
    	match/2 time densities
    mean
    	
    1
    2
    2
    3
    3
    4
    4
    0.5 iters
    5
    10
    15
    20
    25
    30
    35
    0 s
    	regression
    	match/2 times
    	lower bound 	estimate 	upper bound
    OLS regression 	5.27 s 	7.77 s 	9.53 s
    R² goodness-of-fit 	0.960 	0.989 	1.000
    Mean execution time 	7.78 s 	8.13 s 	8.33 s
    Standard deviation 	0 s 	309 ms 	336 ms
    
    Outlying measurements have moderate (18.7%) effect on estimated standard deviation.
    
    See re1.html for details.

---
+++ 010-withtests.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module Main where
+
+import Test.QuickCheck
+import Text.Parsec
+import Text.Regex.TDFA hiding (match)
+import Control.Monad.Identity
+import Data.Either
+import Control.Applicative hiding (many)
+import Criterion.Main
+
+dot = do
+  _ <- anyChar
+  return ()
+
+regular c = do
+  _ <- char c
+  return ()
+
+star c = do
+  let p = if c == '.' then anyChar else char c
+  _ <- many p
+  return ()
+
+compileHelper = sequence_ . reverse . go . reverse
+  where go [] = []
+        go  (x:xs)
+          | x == '.' = dot : go xs
+          | x == '*' = star (head xs) : go (tail xs)
+          | otherwise = regular x : go xs
+
+compile s = compileHelper s >> eof
+
+match :: String -> String -> Bool
+match s p = isRight . runIdentity . runParserT (compile p) 0 "<stdin>" $ s
+
+data Match = Match String String deriving Show
+
+instance Arbitrary Match where
+  arbitrary = liftA2 Match s p4
+    where s = fmap (take 10000) (infiniteListOf (elements['a'..'z']))
+          p1 = fmap (take 1000) (infiniteListOf (elements ('.' : ['a'..'z'])))
+          c1 = elements ('.':['a'..'z'])
+          c2 = fmap (take 1) (infiniteListOf (elements ('*' : ['a'..'z'])))
+          p2 = liftA2 (:) c1 c2
+          p3 = fmap concat (replicateM 1000 p2)
+          p4 = liftA2 (++) p1 p3
+
+prop1 (Match s p) = (match s p) == (s =~ p)
+
+prop_match_self s = (match s' s') == True
+  where s' = filter (/= '*') s
+
+prop_match_all s = (match s' ".*") == True
+  where s' = filter (/= '*') s
+
+match' :: String -> String -> Bool
+match' s t = s =~ t
+
+testsize=500
+
+main = defaultMain [
+  bgroup "match" [  bench "1" $ whnf (match (replicate testsize 'a')) ('a': replicate (succ testsize) '.')
+                  , bench "2" $ whnf (match' (replicate testsize 'a')) ('a': replicate (succ testsize) '.')
+                 ]
+  ]
