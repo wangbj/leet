9f707577f744a8f77167964e795f4693c0d7697c
Baojun Wang <wangbj@gmail.com>
2016-03-10 13:16:18 -0800

    Create 199.hs
    5 lines vs C 100 lines.

---
+++ 199.hs
@@ -0,0 +1,12 @@
+import Data.Tree
+import Data.Monoid
+import Data.Maybe
+import Control.Monad.Identity
+
+data BTree a = Leaf | Branch a !(BTree a) !(BTree a) deriving Show
+
+ex1 = Branch 1 (Branch 2 Leaf (Branch 5 (Branch 6 Leaf Leaf) Leaf)) (Branch 3 Leaf (Branch 4 Leaf Leaf))
+
+expand Leaf = return (mempty, [])
+expand (Branch a l r) = return (Alt (Just a), [r, l])
+lvl t = sequence . init . map (getAlt . foldMap rootLabel) . takeWhile (not . null) . iterate (concatMap subForest) $ [t]
