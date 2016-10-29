6a6423bbf57d7668c0c8c2f0056e74116f8de66e
Baojun Wang <wangbj@gmail.com>
2016-10-28 19:39:15 -0700

    Update 445.hs

--- 445.hs
+++ 445.hs
@@ -2,6 +2,12 @@ import Control.Applicative
 import Control.Arrow
 import Data.Maybe
 
+(++?) :: Integral a => Maybe a -> Maybe a -> Maybe a
+(++?) a b = (liftA2 (+) a b) <|> a <|> b
+
+tail_ [] = []
+tail_ (x:xs) = xs
+
 add3 c xs ys = maybe [] go s
   where
     s    = c ++? listToMaybe xs ++? listToMaybe ys
